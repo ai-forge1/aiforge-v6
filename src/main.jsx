542e2e6e84c62121e3b52eaa4311a65c37761d54
ai-forge1 <8rpvj9n54w@privaterelay.appleid.com>
2026-09-05 09:37:02 +0200

    Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AiforgeV6AppShell from "./Aiforge_v6_AppShell_v0.2.jsx";
+import "./app.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AiforgeV6AppShell />
+  </React.StrictMode>
+);
